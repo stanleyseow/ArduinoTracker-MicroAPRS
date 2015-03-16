80e0b6aa9725f0b49fa7cd51c77b7d3c91814d93
Luiz Fernando R. Amaral <email@luiz.eng.br>
2015-03-16 00:33:08 -0300

    Added custom comments to APRS packets and fixed the hardcoding of North and East on GPS positions

--- config.h
+++ config.h
@@ -4,13 +4,14 @@
 // Refer to http://wa8lmf.net/aprs/APRS_symbols.htm
 #define SYMBOL_CHAR '('         // Red Car
 #define SYMBOL_TABLE 's'        // Primary Table (s) or Alternate Table (a)
+// Define custom comment for APRS packets
+#define COMMENT "Custom comment"
 // Define your home lat & lon below
 // http://andrew.hedges.name/experiments/convert_lat_long/
 // Goto aprs.fi and find your deg mm.mm ( leave the secs blank )
 // home
 const float HOME_LAT = 3.159777;
 const float HOME_LON = 101.69903;
-
 
 
 
